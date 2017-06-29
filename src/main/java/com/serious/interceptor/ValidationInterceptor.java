453d68a6c4e92c63462bb975d4d7bef268f5ea04
SeriousMa <lovemd@vip.qq.com>
2017-06-29 10:47:04 +0800

    README

--- src/main/java/com/serious/interceptor/ValidationInterceptor.java
+++ src/main/java/com/serious/interceptor/ValidationInterceptor.java
@@ -37,18 +37,25 @@ public class ValidationInterceptor implements ClientInterceptor {
             @Override
             public void sendMessage(ReqT message) {
                 GeneratedMessageV3 messageV3 = (GeneratedMessageV3) message;
-                for (Descriptors.FieldDescriptor fieldDescriptor : messageV3.getDescriptorForType().getFields()) {
-                    if (fieldDescriptor.getOptions().getAllFields().size() > 0) {
-                        doValidate(fieldDescriptor.getFile().getFullName(), fieldDescriptor.getFullName(), messageV3.getField(fieldDescriptor), fieldDescriptor.getOptions());
-                    }
-                }
+                validate(messageV3);
                 super.sendMessage(message);
             }
         };
     }
 
+    private void validate(GeneratedMessageV3 messageV3) {
+        for (Descriptors.FieldDescriptor fieldDescriptor : messageV3.getDescriptorForType().getFields()) {
+            if (messageV3.getField(fieldDescriptor) instanceof GeneratedMessageV3) {
+                GeneratedMessageV3 subMessageV3 = (GeneratedMessageV3) messageV3.getField(fieldDescriptor);
+                validate(subMessageV3);
+            } else if (fieldDescriptor.getOptions().getAllFields().size() > 0) {
+                doValidate(fieldDescriptor.getFile().getFullName(), fieldDescriptor.getFullName(), messageV3.getField(fieldDescriptor), fieldDescriptor.getOptions());
+            }
+        }
+    }
 
-    private void doValidate(String protoName, String fieldName, Object fieldValue, DescriptorProtos.FieldOptions options)throws IllegalArgumentException {
+
+    private void doValidate(String protoName, String fieldName, Object fieldValue, DescriptorProtos.FieldOptions options) throws IllegalArgumentException {
         logger.debug("validate protoName:{},fieldName:{},fieldValue:{}", protoName, fieldName, fieldValue);
         if (!options.getExtension(Validation.max).equals(Validation.max.getDefaultValue())) {
             validatorMap.get(Validation.max.getDescriptor().getFullName()).validate(protoName, fieldName, fieldValue, options.getExtension(Validation.max));
